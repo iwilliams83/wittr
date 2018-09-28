bc7eb3ba9b28c4fbbb96a74c687d4d1a713febc7
Indira Williams <indirajamurath@gmail.com>
2018-09-28 09:20:36 -0400

    done

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,10 +2,10 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).then(function(response) {
       if (response.status === 404) {
-        // TODO: instead, respond with the gif at
-        // /imgs/dr-evil.gif
-        // using a network request
-        return new Response("Whoops, not found");
+
+        return fetch('/imgs/dr-evil.gif')
+
+        //instead of: return new Response(fetch('/imgs/dr-evil.gif'));
       }
       return response;
     }).catch(function() {
